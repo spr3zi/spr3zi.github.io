aabf3654bf531d74d21ef02ac96ca940f82fa5a1
vinny <steven.prezioso@gmail.com>
2024-02-09 02:57:05 +0100

    Added clock/weather framework

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ function randomQuoter() {
 document.addEventListener("DOMContentLoaded", function() {
     randomQuoter();
 });
+
+
+function showTime() {
+    'use strict';
+    var now = new Date(),
+        hours = now.getHours(),
+        minutes = now.getMinutes(),
+        seconds = now.getSeconds();
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    document.getElementById('clock').textContent = hours + ":" + minutes;
+}
+
+
+window.onload = function () {   
+    'use strict';
+    setInterval(showTime, 1000);
+};
